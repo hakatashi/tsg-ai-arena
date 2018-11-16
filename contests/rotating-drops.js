77f3b37523b9f5fa974cdcd9267510b113757939
Koki Takahashi <hakatasiloving@gmail.com>
2018-11-16 19:44:00 +0900

    Implement rotating drops

--- contests/rotating-drops.js
+++ contests/rotating-drops.js
@@ -1,47 +1,109 @@
 const assert = require('assert');
 const countBy = require('lodash/countBy');
+const clamp = require('lodash/clamp');
+const shuffle = require('lodash/shuffle');
+const chunk = require('lodash/chunk');
 
 module.exports.presets = {
-	random: () => (Math.floor(Math.random() * 3) + 1).toString(),
-	clever: (stdin) => {
-		const stones = parseInt(stdin);
-
-		if ((stones - 1) % 4 === 0) {
-			return (Math.floor(Math.random() * 3) + 1).toString();
-		}
-
-		return ((stones - 1) % 4).toString();
-	},
 };
 
 const normalize = (stdout) => {
-	const rawAnswer = parseInt(stdout.toString().trim());
-	const answer = [1, 2, 3].includes(rawAnswer) ? rawAnswer : 3;
-	return answer;
+	const lines = stdout.toString().split('\n');
+	const turns = lines.map((line) => {
+		let matches = null;
+		if (!(matches = line.match(/^(\d+)\s+(\d+)$/))) {
+			return {x: 1, y: 1};
+		}
+		return {
+			x: clamp(parseInt(matches[1]) || 1, 1, Infinity),
+			y: clamp(parseInt(matches[2]) || 1, 1, Infinity),
+		};
+	});
+	return turns;
 };
 
 module.exports.normalize = normalize;
 
+const rotateDrops = (drops, rawX, rawY, {width}) => {
+	const x = rawX - 1;
+	const y = rawY - 1;
+	const clonedDrops = drops.slice();
+	clonedDrops[y * width + x] = drops[(y + 1) * width + x];
+	clonedDrops[y * width + x + 1] = drops[y * width + x];
+	clonedDrops[(y + 1) * width + x + 1] = drops[y * width + x + 1];
+	clonedDrops[(y + 1) * width + x] = drops[(y + 1) * width + x];
+	return clonedDrops;
+};
+
+module.exports.rotateDrops = rotateDrops;
+
+const calculateStore = (rawDrops, {width, height}) => {
+	const chunkSizes = [];
+	const drops = rawDrops.slice();
+
+	while (drops.some((drop) => drop !== null)) {
+		// BFS
+		const start = drops.findIndex((drop) => drop !== null);
+		const color = drops[start];
+		drops[start] = null;
+
+		const queue = [start];
+		let size = 0;
+
+		while (queue.length > 0) {
+			const drop = queue.shift();
+			const x = drop % width;
+			const y = Math.floor(drop / width);
+			if (y - 1 >= 0 && drops[(y - 1) * width + x] === color) {
+				queue.push((y - 1) * width + x);
+				drops[(y - 1) * width + x] = null;
+			}
+			if (y + 1 < height && drops[(y + 1) * width + x] === color) {
+				queue.push((y + 1) * width + x);
+				drops[(y + 1) * width + x] = null;
+			}
+			if (x - 1 >= 0 && drops[y * width + x - 1] === color) {
+				queue.push(y * width + x - 1);
+				drops[y * width + x - 1] = null;
+			}
+			if (x + 1 < width && drops[y * width + x + 1] === color) {
+				queue.push(y * width + x + 1);
+				drops[y * width + x + 1] = null;
+			}
+			size++;
+		}
+
+		chunkSizes.push(size);
+	}
+
+	return chunkSizes.reduce((a, b) => a + b, 0);
+};
+
 module.exports.battler = async (execute, params) => {
+	const count = params.width * params.height;
 	const state = {
-		stones: params.stones,
+		drops: shuffle(Array(count).fill().map((_, i) => (
+			clamp(Math.floor(i / count * 5) + 1, 1, 5)
+		))),
 		turn: 0,
 	};
 
-	while (state.stones >= 1) {
-		const {stdout} = await execute(`${state.stones.toString()}\n`, state.turn);
+	const {stdout} = await execute(`${[
+		`${params.height} ${params.width} ${params.turns}`,
+		...chunk(state.drops, params.width).map((line) => line.join(' ')),
+	].join('\n')}\n`, 0);
+	const turns = normalize(stdout);
 
-		const answer = normalize(stdout);
-
-		state.stones -= answer;
-		if (state.stones >= 1) {
-			state.turn = state.turn === 0 ? 1 : 0;
-		}
+	for (const turn of turns) {
+		state.drops = rotateDrops(state.drops, turn.x, turn.y, params);
 	}
+
+	const score = calculateStore(state.drops, params);
 
 	return {
 		result: 'settled',
-		winner: state.turn === 0 ? 1 : 0,
+		winner: 0,
+		score,
 	};
 };
 
@@ -49,23 +111,11 @@ module.exports.configs = [
 	{
 		default: true,
 		id: 'small',
-		name: '24 Stones',
+		name: '5 x 5',
 		params: {
-			stones: 24,
-		},
-	},
-	{
-		id: 'medium',
-		name: '50 stones',
-		params: {
-			stones: 50,
-		},
-	},
-	{
-		id: 'big',
-		name: '100 stones',
-		params: {
-			stones: 100,
+			height: 5,
+			width: 5,
+			turns: 20,
 		},
 	},
 ];
@@ -73,15 +123,7 @@ module.exports.configs = [
 module.exports.matchConfigs = [
 	{
 		config: 'small',
-		players: [0, 1],
-	},
-	{
-		config: 'medium',
-		players: [0, 1],
-	},
-	{
-		config: 'big',
-		players: [0, 1],
+		players: [0],
 	},
 ];
 
